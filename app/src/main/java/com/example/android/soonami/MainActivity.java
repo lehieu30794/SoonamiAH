6824d9ce7551eddb8da950e3c5f0cddba18774a2
lehieu30794 <lehieu30794@gmail.com>
2018-08-09 11:36:01 -0600

    Log errors

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -179,9 +179,11 @@ public class MainActivity extends AppCompatActivity {
                 if (urlConnection.getResponseCode() == 200) {
                     inputStream = urlConnection.getInputStream();
                     jsonResponse = readFromStream(inputStream);
+                } else {
+                    Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
                 }
             } catch (IOException e) {
-                // TODO: Handle the exception
+                Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results: ", e);
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
@@ -201,7 +203,9 @@ public class MainActivity extends AppCompatActivity {
         private String readFromStream(InputStream inputStream) throws IOException {
             StringBuilder output = new StringBuilder();
             if (inputStream != null) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                // InputStreamReader only allow read one character at a time
+                InputStreamReader inputStreamReader =
+                        new InputStreamReader(inputStream, Charset.forName("UTF-8"));
                 BufferedReader reader = new BufferedReader(inputStreamReader);
                 String line = reader.readLine();
                 while (line != null) {
